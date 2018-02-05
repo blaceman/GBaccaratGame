d3e7c021f616f6664f9a1ac11f421faf9d461be4
luowx <luowx@figo.cn>
2018-02-05 11:18:04 +0800

    Initial Commit

---
+++ GBaccaratGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GBaccaratGame
+//
+//  Created by blaceman on 2018/2/5.
+//  Copyright © 2018年 new4545. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GBaccaratGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GBaccaratGame
+//
+//  Created by blaceman on 2018/2/5.
+//  Copyright © 2018年 new4545. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
